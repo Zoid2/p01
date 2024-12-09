a3bb4423af44f4672e4d4e3f7268ab06e8bb7a7f
chloepwong <cwong50@stuy.edu>
2024-12-08 19:54:48 -0500

    added key handling with tests

--- app/__init__.py
+++ app/__init__.py
@@ -10,10 +10,26 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 import db_helpers as db
+import urllib.request
+import json
 
 app = Flask(__name__) 
 secret = os.urandom(32)
 app.secret_key = secret
+
+key_merriam = None
+try:
+    with open("keys/key_merriam_webster.txt", "r") as file:
+        key_merriam = file.read().strip()
+except:
+    print('no merriam key')
+
+key_unsplash = None
+try:
+    with open("keys/key_unsplash.txt", "r") as file:
+        key_unsplash = file.read().strip()
+except:
+    print('no unsplash key')
 
 @app.route("/")
 def home():
@@ -58,6 +74,29 @@ def logout():
 @app.route("/signup")
 def signup():
     return render_template("signup.html", projectName = "Name PH")
+
+@app.route("/lesson")
+def lesson():
+    datamuse = urllib.request.urlopen('https://api.datamuse.com/words?ml=food')
+    datamuse_data = json.loads(datamuse.read())
+    word = datamuse_data[0]['word']
+    translation = None
+    if key_merriam:
+        try:
+            merriam = urllib.request.urlopen('https://www.dictionaryapi.com/api/v3/references/spanish/json/' + word + '?key=' + key_merriam)
+            merriam_data = json.loads(merriam.read())
+            translation = merriam_data[0]['shortdef'][0]
+        except:
+            print('error with merriam api')
+    image = None
+    if key_unsplash:
+        try:
+            unsplash = urllib.request.urlopen('https://api.unsplash.com/search/photos?page=1&query=' + word + '&client_id=' + key_unsplash)
+            unsplash_data = json.loads(unsplash.read())
+            image = unsplash_data['results'][0]['urls']['raw']
+        except:
+            print('error with unsplash api')
+    return render_template('lesson.html', word = word, translation = translation, image = image)
 
 if __name__ == "__main__":
     app.debug = True
